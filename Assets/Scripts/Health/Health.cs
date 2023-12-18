44013c3da3e8d528abfbae6e34d83c5e3f8d5547
DavronbekA <dawiut2017@gmail.com>
2023-12-17 23:26:35 -0500

    Initial commit

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -13,6 +13,10 @@ public class Health : MonoBehaviour
     [SerializeField] private float iFrameDuration;
     [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
+
+    [Header("Death Sound")]
+    [SerializeField] private AudioClip deathSound;
+    [SerializeField] private AudioClip hurtSound;
     private void Awake()
     {
         currentHealth = startingHealth;
@@ -36,6 +40,7 @@ public class Health : MonoBehaviour
                 anim.SetTrigger("die");
                 GetComponent<PlayerMovement>().enabled = false;
                 dead = true;
+                SoundManager.instance.PlaySound(deathSound);
             }
         }
     }
